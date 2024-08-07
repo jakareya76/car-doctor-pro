32d62024439d76628bff89795fc3670dce0960b3
jakareya76 <jakareya1306@gmail.com>
2024-08-07 22:38:18 +0600

    make card button link

--- src/components/Cards/ServiceCard.jsx
+++ src/components/Cards/ServiceCard.jsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 import { FaArrowRight } from "react-icons/fa6";
 
 const ServiceCard = ({ service }) => {
-  const { img, title, price } = service || {};
+  const { _id, img, title, price } = service || {};
   return (
     <div className="p-5 border rounded-xl">
       <Image
@@ -15,9 +16,12 @@ const ServiceCard = ({ service }) => {
       <h2 className="text-2xl text-gray-700 font-bold my-3">{title}</h2>
       <div className="flex items-center justify-between">
         <h4 className="text-xl text-primary font-bold">Price: ${price}</h4>
-        <div className="bg-primary p-2 rounded-full cursor-pointer">
+        <Link
+          href={`/services/${_id}`}
+          className="bg-primary p-2 rounded-full cursor-pointer"
+        >
           <FaArrowRight color="white" size={20} />
-        </div>
+        </Link>
       </div>
     </div>
   );
